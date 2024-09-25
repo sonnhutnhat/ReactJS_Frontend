c795605ca1d38b9082ea27477e66f96655a6d3c7
sonnhutnhat <nguyenkienson3010@gmail.com>
2024-09-25 21:17:08 +0700

    Design HomePage, Home Header

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,6 +19,8 @@ import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 
+import HomePage from './HomePage/HomePage.js'
+
 class App extends Component {
 
     handlePersistorState = () => {
@@ -52,6 +54,7 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={(HomePage)} />
                             </Switch>
                         </span>
 
